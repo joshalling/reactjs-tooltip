de041424151aaac9421b8594eadcc9a3b035fd91
Josh Alling <joshralling@gmail.com>
2018-12-13 14:46:59 -0700

    add tooltip component

---
+++ src/styles.js
@@ -0,0 +1,47 @@
+export default {
+    container: {
+        position: "relative",
+        padding: "4px"
+    },
+    tooltip: {
+        position: "absolute",
+        zIndex: 1,
+        borderRadius: "3px",
+        padding: "8px",
+        width: "250px",
+        textAlign: "left",
+        backgroundColor: "rgba(82, 82, 82, 0.95)",
+        color: "#fff"
+    },
+    positioner: (contentEl, placement) => {
+        console.log(contentEl);
+        const content = contentEl.getBoundingClientRect();
+        switch (placement) {
+            case "top":
+                return {
+                    bottom: "100%",
+                    left: `calc(50% - ${content.width / 2}px)`
+                };
+            case "right":
+                return {
+                    top: `calc(50% - ${content.height / 2}px)`,
+                    left: "100%"
+                };
+            case "bottom":
+                return {
+                    top: "100%",
+                    left: `calc(50% - ${content.width / 2}px)`
+                };
+            case "left":
+                return {
+                    top: `calc(50% - ${content.height / 2}px)`,
+                    right: "100%"
+                };
+            default:
+                return {
+                    top: "100%",
+                    left: `calc(50% - ${content.width / 2}px)`
+                };
+        }
+    }
+};
