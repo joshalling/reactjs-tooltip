75ca92b895b9fda53d2ee895aae2ff727a2aa469
Josh Alling <joshralling@gmail.com>
2020-10-01 08:46:54 -0600

    add proptypes

--- src/index.js
+++ src/index.js
@@ -1,47 +1,57 @@
+import PropTypes from "prop-types";
 import React, { Component } from "react";
 import styles from "./styles";
 
 class Tooltip extends Component {
-    state = {
-        isOpen: false,
-        position: {}
-    };
+  state = {
+    isOpen: false,
+    position: {},
+  };
 
-    _handleMouseIn = () => {
-        this.setState(() => ({ isOpen: true }));
-    };
+  _handleMouseIn = () => {
+    this.setState(() => ({ isOpen: true }));
+  };
 
-    _handleMouseOut = () => {
-        this.setState(() => ({ isOpen: false }));
-    };
+  _handleMouseOut = () => {
+    this.setState(() => ({ isOpen: false }));
+  };
 
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.isOpen !== this.state.isOpen && this.state.isOpen === true) {
-            this.setState(() => ({
-                position: styles.positioner(this.contentRef, this.props.placement)
-            }));
-        }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.isOpen !== this.state.isOpen && this.state.isOpen === true) {
+      this.setState(() => ({
+        position: styles.positioner(this.contentRef, this.props.placement),
+      }));
     }
+  }
 
-    render() {
-        return (
-            <span
-                style={{ ...styles.container, ...this.props.style }}
-                onMouseOver={this._handleMouseIn}
-                onMouseOut={this._handleMouseOut}
-            >
-                {this.props.children}
-                {this.state.isOpen && (
-                    <div
-                        style={{ ...styles.tooltip, ...this.state.position }}
-                        ref={ref => (this.contentRef = ref)}
-                    >
-                        {this.props.content}
-                    </div>
-                )}
-            </span>
-        );
-    }
+  render() {
+    return (
+      <span
+        style={{ ...styles.container, ...this.props.style }}
+        onMouseOver={this._handleMouseIn}
+        onMouseOut={this._handleMouseOut}
+      >
+        {this.props.children}
+        {this.state.isOpen && (
+          <div
+            style={{ ...styles.tooltip, ...this.state.position }}
+            ref={(ref) => (this.contentRef = ref)}
+          >
+            {this.props.content}
+          </div>
+        )}
+      </span>
+    );
+  }
 }
+
+Tooltip.propTypes = {
+  content: PropTypes.node.isRequired,
+  placement: PropTypes.oneOf(["top", "right", "bottom", "left"]),
+};
+
+Tooltip.defaultProps = {
+  placement: "bottom",
+};
 
 export default Tooltip;
