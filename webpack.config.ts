279f8bb2195b50ca5938d03f68d44ce89450d1ff
Josh Alling <joshralling@gmail.com>
2023-10-25 08:06:32 -0700

    fix webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,27 +1,25 @@
-var path = require("path");
+var path = require('path');
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'production',
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "index.js",
-    libraryTarget: "commonjs2",
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.js$/,
-        include: path.resolve(__dirname, "src"),
-        exclude: /(node_modules|build)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: ["babel-plugin-transform-class-properties"],
-          },
-        },
+        test: /\.(js|jsx|ts|tsx)$/, // USE THE babel-loader FOR THESE FILE EXTENSIONS
+        include: path.resolve(__dirname, 'src'),
+        use: ['babel-loader'],
       },
     ],
   },
   externals: {
-    react: "commonjs react",
+    react: 'commonjs react',
   },
 };
